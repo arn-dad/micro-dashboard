10c093d83845425ae3e1b34d73fe8bbd92c66a9e
Arnold Dadayan <arnold@YLT190020.corp.optym.net>
2021-01-10 01:05:20 +0400

    Created container deploy script

--- packages/container/config/webpack.dev.js
+++ packages/container/config/webpack.dev.js
@@ -1,6 +1,5 @@
 const { merge } = require('webpack-merge');
 const ModuleFederationPlugin = require('webpack/lib/container/ModuleFederationPlugin');
-const HtmlWebpackPlugin = require('html-webpack-plugin');
 const commonConfig = require('./webpack.common');
 const packageJSON = require('../package.json');
 
